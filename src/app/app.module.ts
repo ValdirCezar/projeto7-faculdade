4db319f8bf718a8ba0b9c6b829beb7b77d60c330
Valdir Cezar <valdircezar312@gmail.com>
2021-06-10 20:41:26 -0300

    Cards finalizados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+
+import { HeaderComponent } from './components/header/header.component';
 import { ContentComponent } from './components/content/content.component';
 
 @NgModule({
@@ -21,7 +24,9 @@ import { ContentComponent } from './components/content/content.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    MatButtonModule
+    MatButtonModule,
+    MatCardModule,
+    MatGridListModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
